c5ddbbf3665eb7d9901454b52d09937bf79b27e8
Pedronpm <pedronogueirapm@gmail.com>
2023-12-01 15:20:11 -0300

    Update ControleLocacao.cpp

--- ControleLocacao.cpp
+++ ControleLocacao.cpp
@@ -6,3 +6,28 @@
 #include <map>
 #include "CadastroFilmes.h"
 #include "CadastroClientes.h"
+
+ControleLocacao::ControleLocacao(CadastroFilmes& filmes, CadastroClientes& clientes)
+    : cadastroFilmes(filmes), cadastroClientes(clientes) {}
+
+void ControleLocacao::alugarFilme(const std::string& cpf, const std::vector<std::string>& codigosFilmes) {
+    if (!clienteExiste(cpf)) {
+        exibirErro("CPF inexistente");
+        return;
+    }
+
+    FilmeCodes filmesAlugados;
+    std::string nomeCliente = cadastroClientes.getNomeCliente(cpf);
+
+    for (const auto& codigo : codigosFilmes) {
+        if (!filmeExiste(codigo)) {
+            exibirErro("Filme inexistente: " + codigo);
+        } else {
+            filmesAlugados.push_back(codigo);
+        }
+    }
+
+    locacoes[cpf] = filmesAlugados;
+
+    exibirMensagem("Cliente " + cpf + " " + nomeCliente + " alugou os filmes:", filmesAlugados);
+}
