fe830d069a18c5991d511d0700487b06e7fdc854
Pedronpm <pedronogueirapm@gmail.com>
2023-12-01 14:06:09 -0300

    locação .cpp e .h

--- ControleLocacao.cpp
+++ ControleLocacao.cpp
@@ -1,1 +1,1 @@
-
+#include "ControleLocacao.h"
--- ControleLocacao.h
+++ ControleLocacao.h
@@ -1,1 +1,8 @@
+#ifndef CONTROLELOCACAO_H
+#define CONTROLELOCACAO_H
 
+#include <iostream>
+#include <vector>
+#include <map>
+#include "CadastroFilmes.h"
+#include "CadastroClientes.h"
