6dd791ced38ea1e586b3005c5b874a21d52ec8a1
Pedronpm <pedronogueirapm@gmail.com>
2023-12-01 14:11:58 -0300

    update locação .cpp e .h

--- ControleLocacao.cpp
+++ ControleLocacao.cpp
@@ -1,1 +1,2 @@
 #include "ControleLocacao.h"
+#include <iomanip>
--- ControleLocacao.h
+++ ControleLocacao.h
@@ -6,3 +6,21 @@
 #include <map>
 #include "CadastroFilmes.h"
 #include "CadastroClientes.h"
+
+class ControleLocacao {
+private:
+    std::map<std::string, std::vector<std::string>> locacoes; // CPF -> Lista de códigos de filmes alugados
+    CadastroFilmes& cadastroFilmes;
+    CadastroClientes& cadastroClientes;
+
+public:
+    ControleLocacao(CadastroFilmes& filmes, CadastroClientes& clientes);
+
+    void alugarFilme(const std::string& cpf, const std::vector<std::string>& codigosFilmes);
+    void devolverFilme(const std::string& cpf);
+    void imprimirReciboAluguel(const std::string& cpf);
+    void imprimirReciboDevolucao(const std::string& cpf);
+    void imprimirRelatorioLocacoesEmCurso();
+};
+
+#endif // CONTROLELOCACAO_H
