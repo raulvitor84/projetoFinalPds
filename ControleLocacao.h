677d5336c381674e06f3fc0ecea8c66138405933
Pedronpm <pedronogueirapm@gmail.com>
2023-12-01 14:45:13 -0300

    Revert "updade locação .cpp construtor"
    This reverts commit f73f4d246500f37e3593f340ef2573a7f88a72d1.

--- ControleLocacao.h
+++ ControleLocacao.h
@@ -1,5 +1,30 @@
-#include "ControleLocacao.h"
-#include <iomanip>
+// ControleLocacao.h
+#ifndef CONTROLELOCACAO_H
+#define CONTROLELOCACAO_H
 
-ControleLocacao::ControleLocacao(CadastroFilmes& filmes, CadastroClientes& clientes)
-    : cadastroFilmes(filmes), cadastroClientes(clientes) {}
+#include <string>
+#include <vector>
+#include <map>
+#include "CadastroFilmes.h"
+#include "CadastroClientes.h"
+
+class ControleLocacao {
+public:
+    ControleLocacao(CadastroFilmes& filmes, CadastroClientes& clientes);
+
+    void alugarFilme(const std::string& cpf, const std::vector<std::string>& codigosFilmes);
+    void devolverFilme(const std::string& cpf);
+    void imprimirReciboAluguel(const std::string& cpf);
+    void imprimirReciboDevolucao(const std::string& cpf);
+    void imprimirRelatorioLocacoesEmCurso();
+
+private:
+    using FilmeCodes = std::vector<std::string>;
+    using LocacoesMap = std::map<std::string, FilmeCodes>;
+
+    LocacoesMap locacoes; // CPF -> Lista de códigos de filmes alugados
+    CadastroFilmes& cadastroFilmes;
+    CadastroClientes& cadastroClientes;
+};
+
+#endif // CONTROLELOCACAO_H
