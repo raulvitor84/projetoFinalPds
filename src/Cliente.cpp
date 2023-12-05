867661d7236fb45b49b1a075aa5a9e1b6b625d11
Leonardofd <48093642+Leonardofd@users.noreply.github.com>
2023-12-05 06:19:31 -0300

    inc Try catch Cliente.cpp

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -1,5 +1,8 @@
 #include "Cliente.h"
 
+#include <stdexcept>
+#include <iostream>
+
 Cliente::Cliente(string nome, string cpf){
 	this->_nome = nome;
 	this->_cpf = cpf;
@@ -18,7 +21,16 @@ string Cliente::getCPF()
 }
 
 void Cliente::setNome(string nome){
+  try {
+        if (nome.empty()) {
+            throw invalid_argument("Nome nao pode ser vazio. ");
+        }
+
 	this->_nome = nome;
+   } catch (const exception& e) {
+        cerr << "Erro: " << e.what() << endl;
+
+}
 }
 
 void Cliente::setCPF(string cpf){
