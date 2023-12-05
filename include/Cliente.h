9ebfa232664c376d7d1ddd878d9ba4d396f2aa17
Leonardofd <48093642+Leonardofd@users.noreply.github.com>
2023-12-05 01:07:01 -0300

    doxy comments Cliente.h

--- include/Cliente.h
+++ include/Cliente.h
@@ -5,20 +5,54 @@
 
 using namespace std;
 
+/**
+ * @brief Classe Cliente representa um cliente no sistema
+ */
+ 
 class Cliente
 {
 	public:
+    /**
+     * @brief Construtor da classe Cliente.
+     * @param Nome do cliente.
+     * @param CPF do cliente.
+     */
 		Cliente(string nome, string cpf);
-		virtual ~Cliente();
+		
+    /**
+     * @brief Destrutor da classe Cliente.
+     */
+    	virtual ~Cliente();
 
-		string getNome();
+    /**
+     * @brief Obtém o nome do cliente.
+     * @return Nome 
+     */
+    	string getNome();
+    	
+    /**
+     * @brief Obtém o CPF do cliente.
+     * @return CPF 
+     */    	
 		string getCPF();
+		
+		
+    /**
+     * @brief Insere o nome do cliente.
+     * @param Nome 
+     */
 		void setNome(string);
+		
+    /**
+     * @brief Insere o CPF do cliente.
+     * @param CPF 
+     */		
 		void setCPF(string);
 
     private:
-        string _nome;
-        string _cpf;
+        string _nome;///< Nome do Cliente
+        string _cpf;///< CPF do Cliente
 };
 
 #endif // CLIENTE_H
+
