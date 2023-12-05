6a6417719500e255ea8b1425e128ee3a8093d020
Leonardofd <48093642+Leonardofd@users.noreply.github.com>
2023-12-05 03:40:54 -0300

    Update testes.cpp

--- testes.cpp
+++ testes.cpp
@@ -1,13 +1,13 @@
 // arquivo de teste (testes.cpp)
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "Clientes.h"
-#include "Filmes.h"
-#include "Locacao.h"
+#include "Cliente.h"
+#include "Filme.h"
+#include "Aluguel.h"
 
 
-TEST_CASE("Teste de Clientes") {
-    Clientes cadastro;
+TEST_CASE("Teste de Cliente") {
+    Cliente cadastro;
 
     cadastro.Cliente(911222, "Raul Vitor");
     CHECK(cadastro.getCPF() == 911222);
@@ -22,16 +22,14 @@ TEST_CASE("Teste de Clientes") {
     CHECK(cadastro.getCPF() == 101011);
 
 
-    cadastro.Cliente(911222);
-    CHECK(cadastro.//clienteExistente(911222) == false);
+    cadastro.removeCliente(911222);
+    CHECK(cadastro.getCPF() != 911222);
 
     cadastro.removeCliente(25896);
-    CHECK(cadastro.//clienteExistente(25896) == false);
+    CHECK(cadastro.getCPF() != 25896);
 
 
-    cadastro.removeCliente(000145);
-    CHECK(cadastro.//clienteExistente(1010) == true);
-
+ 
 }
 
 
@@ -39,52 +37,52 @@ TEST_CASE("Teste de Clientes") {
 
 
 
-TEST_CASE("Teste de Filmes") {
-    Filmes cadastro;
+TEST_CASE("Teste de Filme") {
+    Filme cadastro;
 
-    cadastro.cadastraFilme('D', 12, 0000010, "O Rei Leao", "lancamentos");
-    CHECK(cadastro.//codigoDeFilmeExistente(0000010) == true);
-
-
-    cadastro.cadastraFilme('D', 7, 202020, "O Lobo de Wall Street", "estoque");
-    CHECK(cadastro.//codigoDeFilmeExistente(202020) == true);
+    cadastro.Filme('D', 12, 0000010, "O Rei Leao", "lancamentos");
+    CHECK(cadastro.getCodigo() == true);
 
 
-    cadastro.cadastraFilme('D', 8, 202021, "Wall-E", "estoque");
-    CHECK(cadastro.//codigoDeFilmeExistente(202020) == true);
+    cadastro.Filme('D', 7, 202020, "O Lobo de Wall Street", "estoque");
+    CHECK(cadastro.getCodigo() == true);
 
 
-    cadastro.cadastraFilme('F', 2, 10101, "Mercenarios", "estoque");
-    CHECK(cadastro.//codigoDeFilmeExistente(0101) == false);
+    cadastro.Filme('D', 8, 202021, "Wall-E", "estoque");
+    CHECK(cadastro.getCodigo() == true);
+
+
+    cadastro.Filme('F', 2, 10101, "Mercenarios", "estoque");
+    CHECK(cadastro.getCodigo() == false);
 
     cadastro.removeFilme(10100);
-    CHECK(cadastro.//codigoExistente(10100) == false);
+    CHECK(cadastro.getCodigo() == false);
 
     cadastro.removeFilme(202020);
-    CHECK(cadastro.//codigoExistente(202020) == false);
+    CHECK(cadastro.getCodigo() == false);
 
     cadastro.removeFilme(000);
-    CHECK(cadastro.//codigoExistente(0000010) == true);
+    CHECK(cadastro.getCodigo() == true);
 
 }
 
 
-TEST_CASE("Teste de Locacao") {
-    Locacao locacao;
+TEST_CASE("Teste de Aluguel") {
+    Aluguel locacao;
 
 
     locacao.alugaFilme(1010, {10101, 0000010, 202021});
-    CHECK(locacao.//filmesAlugados.size() == 3);
+    CHECK(locacao._alugueis.size() == 3);
 
 
     locacao.alugaFilme(101011, {0000010, 202021});
-    CHECK(locacao.//filmesAlugados.size() == 2);
+    CHECK(locacao._alugueis.size() == 2);
 
     locacao.devolveFilme(101011);
-    CHECK(locadora.filmesAlugados.empty() == true);
+    CHECK(locacao._alugueis.empty() == true);
 
     locacao.devolveFilme(1010);
-    CHECK(locadora.filmesAlugados.empty() == true);
+    CHECK(locacao._alugueis.empty() == true);
 
 }
 
