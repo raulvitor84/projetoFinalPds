cbeb5e908792603835ec0a2815c95c1127f4ee8c
Leonardofd <48093642+Leonardofd@users.noreply.github.com>
2023-12-05 00:55:28 -0300

    Comentarios doxy Filme.h

--- include/Filme.h
+++ include/Filme.h
@@ -10,23 +10,70 @@ using namespace std;
 class Filme
 {
 	public:
+    /**
+     * @brief Construtor da classe Filme.
+     * @param codigo (Código do filme).
+     * @param nome (Nome do filme).
+     * @param tipos (Vetor de tipos de mídia).
+     */
 		Filme(string codigo, string nome, vector<Midia> *tipos = nullptr);
+
+    /**
+     * @brief Destrutor da classe Filme.
+     */
 		virtual ~Filme();
 
+    /**
+     * @brief Obtém o código do filme.
+     * @return Código do filme.
+     */
 		string getCodigo();
+	/**
+
+     * @brief Obtém o nome do filme.
+     * @return Nome do filme.
+     */
         string getNome();
+
+    /**
+     * @brief Obtém os tipos de mídia do filme.
+     * @return Vetor de tipos de mídia.
+     */
         vector<Midia> getTipos();
+
+    /**
+     * @brief Obtém a quantidade total de mídias de um filme
+     * @return Quantidade total de mídias
+     */
         int getQuantidade();
 
+
+    /**
+     * @brief Define o código do filme
+     * @param Código do filme
+     */
+
 		void setCodigo(string);
+
+    /**
+     * @brief Adiciona um tipo de midia ao filme
+     * @param Tipo de mídia a ser adicionado
+     * @note Função também atualiza a quantidade total de mídias do filme
+     */
         void addTipos(Midia);
+
+
+    /**
+     * @brief Define o nome do filme.
+     * @param Nome do filme.
+     */
         void setNome(string);
 
     private:
-        string _codigo;
-        vector<Midia> _tipos;
-        string _nome;
-        int _quantidade;
+        string _codigo; ///< Codigo do filme
+        vector<Midia> _tipos;///< Vetor de tipos de mídia
+        string _nome;///< Nome do Filme
+        int _quantidade;///< Quantidade de midias do filme em estoque
 };
 
 #endif // FILME_H
